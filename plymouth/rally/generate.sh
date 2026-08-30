540dfedb55f24e7ca8a23c2a46473fac298da084
SirJul1337 <84667145+SirJul1337@users.noreply.github.com>
2026-08-30 17:08:57 +0200

    Remove the Rally boot generator

--- plymouth/rally/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Boot screen twin of the rally clip design, see cliptwin.sh.
-exec bash "$(dirname "$(realpath "$0")")/../cliptwin.sh" "${1:?usage: generate.sh <staging-dir>}" rally omarchy-rally.mp4
